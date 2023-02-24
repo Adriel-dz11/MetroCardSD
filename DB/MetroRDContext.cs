70942585437c423107d69358ecad59f6e05e58e1
Alexander Coronado <107020325+AlexCLDev@users.noreply.github.com>
2023-02-24 10:48:16 -0400

    Revert "Front init"

---
+++ DB/MetroRDContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DB
+{
+    public class MetroRDContext : DbContext
+    {
+        public MetroRDContext(DbContextOptions<MetroRDContext> options) : base(options)
+        {
+
+        }
+    }
+}
