6b8e63e404df2ab02c95e8762552fd0a4f5b9e96
Adriel <adrielenmanuel11@gmail.com>
2023-04-09 02:37:02 -0400

    CReacion del modelo y Controller de ExternalCards

---
+++ DB/Models/ExternalCards.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB.Models
+{
+    public class ExternalCards
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int CardID { get; set; }
+        [Required]
+        public string CardNumber { get; set; }
+        public int Amount { get; set; }
+        public string Currency { get; set; }
+        public string CardType { get; set; }
+        public string Description { get; set; }
+       
+        [ForeignKey("UserID")]
+        public virtual Users Users { get; set; }
+    }
+}
---
+++ WebApi/Controllers/ExternalCardsController.cs
@@ -0,0 +1,65 @@
+﻿using DB;
+using DB.Models;
+using Microsoft.AspNetCore.Mvc;
+using Stripe;
+using System;
+
+namespace WebApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ExternalCardsController : ControllerBase
+    {
+        private readonly MetroCardContext _context;
+
+        public ExternalCardsController(MetroCardContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost("process-payment")]
+        public IActionResult ProcessPayment([FromBody] PaymentRequest paymentRequest)
+        {
+            StripeConfiguration.ApiKey = "sk_test_51MupeTGQwZvL7vth6DwJkHUGS4P8yBm9DVZUTSLwcUMJDuNLlBUV3TljTIiStAlnLrdMoX7G6qUF0gF7QurV7EKc00hYrJ2LHD";
+
+            var chargeOptions = new ChargeCreateOptions
+            {
+                Amount = paymentRequest.Amount,
+                Currency = paymentRequest.Currency,
+                Description = paymentRequest.Description,
+                Source = paymentRequest.TokenId,
+            };
+
+            var chargeService = new ChargeService();
+            var charge = chargeService.Create(chargeOptions);
+
+            if (charge.Status == "succeeded")
+            {
+                var payment = new ExternalCards
+                {
+                    Description = paymentRequest.Description,
+                    Amount = paymentRequest.Amount,
+                    Currency = paymentRequest.Currency,
+                   
+                };
+
+                _context.Add(payment);
+                _context.SaveChanges();
+
+                return Ok();
+            }
+            else
+            {
+                return BadRequest();
+            }
+        }
+    }
+
+    public class PaymentRequest
+    {
+        public string Description { get; set; }
+        public int Amount { get; set; }
+        public string Currency { get; set; }
+        public string TokenId { get; set; }
+    }
+}
